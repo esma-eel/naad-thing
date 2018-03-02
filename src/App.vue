068504a47e87e87bd0cd7e81b73c40cdaf5b6300
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-03-02 15:42:41 +0330

    added animated.css and fadeIn/Out animation in routed --96/12/11

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,39 @@
       <div class="column is-one-fifth ">
         <my-menu></my-menu>
       </div>
-<!-- remember to write and config css class color again
+      <!-- remember to write and config css class color again
   and some padding and margin classes
  -->
-      <div class="column  is-four-fifths "> 
+      <div class="column  is-four-fifths ">
         <my-navbar></my-navbar>
-        <br><br>
-        <router-view class="my-main-padding"/>
+        <br>
+        <br>
+        <transition 
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+        mode="out-in"
+        >
+          <router-view class="my-main-padding" />
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Menu from '@/components/Menu.vue'
-import NavBar from '@/components/NavBar.vue'
+  import Menu from '@/components/Menu.vue'
+  import NavBar from '@/components/NavBar.vue'
 
-export default {
-  created() {
-    this.$store.dispatch('initLessonData');
-    this.$store.dispatch('initlessonColumns');
+  export default {
+    created() {
+      this.$store.dispatch('initLessonData');
+      this.$store.dispatch('initlessonColumns');
+    },
+    components: {
+      'my-menu': Menu,
+      'my-navbar': NavBar,
+    }
   }
-  ,
-  components: {
-    'my-menu': Menu,
-    'my-navbar': NavBar,
-  }
-}
 </script>
 
 <style lang="scss">
@@ -38,56 +44,54 @@ export default {
     padding: 1%;
   }
 
-  .my-white{
-    background-color:  hsl(0, 0%, 100%)  !important;
-    color:  hsl(0, 0%, 4%)  !important;
+  .my-white {
+    background-color: hsl(0, 0%, 100%) !important;
+    color: hsl(0, 0%, 4%) !important;
 
   }
 
   .my-black {
     background-color: hsl(0, 0, 4%) !important;
-    color:   hsl(0, 0%, 100%)   !important;
+    color: hsl(0, 0%, 100%) !important;
   }
 
   .my-light {
-    background-color : hsl(0, 0, 96%) !important;
-    color:   	hsl(0, 0%, 21%)  !important;
+    background-color: hsl(0, 0, 96%) !important;
+    color: hsl(0, 0%, 21%) !important;
   }
 
   .my-dark {
-    background-color :  hsl(0, 0%, 21%) !important; 
-    color:  hsl(0, 0%, 96%)  !important;
+    background-color: hsl(0, 0%, 21%) !important;
+    color: hsl(0, 0%, 96%) !important;
   }
 
   .my-primary {
-    background-color :  hsl(171, 100%, 41%)  !important;
-    color:   hsl(0, 0%, 100%)   !important;
+    background-color: hsl(171, 100%, 41%) !important;
+    color: hsl(0, 0%, 100%) !important;
   }
 
   .my-link {
-    background-color :  hsl(217, 71%, 53%) !important;
-    color:  hsl(0, 0%, 100%)   !important;
+    background-color: hsl(217, 71%, 53%) !important;
+    color: hsl(0, 0%, 100%) !important;
   }
 
   .my-info {
-    background-color :   hsl(204, 86%, 53%)  !important;
-    color:   hsl(0, 0%, 100%)   !important;
+    background-color: hsl(204, 86%, 53%) !important;
+    color: hsl(0, 0%, 100%) !important;
   }
 
   .my-success {
-    background-color :   hsl(141, 71%, 48%)  !important;
-    color:   hsl(0, 0%, 100%)  !important;
+    background-color: hsl(141, 71%, 48%) !important;
+    color: hsl(0, 0%, 100%) !important;
   }
 
   .my-warning {
-    background-color :   hsl(48, 100%, 67%)  !important;
-    color:   rgba(0, 0, 0, 0.7)   !important;
+    background-color: hsl(48, 100%, 67%) !important;
+    color: rgba(0, 0, 0, 0.7) !important;
   }
 
   .my-danger {
-    background-color :   hsl(348, 100%, 61%)  !important;
-    color:  hsl(0, 0%, 100%)  !important;
+    background-color: hsl(348, 100%, 61%) !important;
+    color: hsl(0, 0%, 100%) !important;
   }
-
 </style>
-
