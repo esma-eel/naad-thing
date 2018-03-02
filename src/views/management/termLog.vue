b59d50df126d66a695d7ab9e6d083bc2eac4cbda
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-03-02 18:59:51 +0330

    added karnameh component -- 96/12/11

--- src/views/management/termLog.vue
+++ src/views/management/termLog.vue
@@ -1,12 +1,58 @@
 <template>
     <div>
-        <h1 class="is-size-1">TermLog.vue</h1>
+        <table class="table  table is-fullwidth has-text-centered">
+            <thead>
+                <tr class="has-text-centered">
+                    <th class="has-text-centered">کد درس</th>
+                    <th class="has-text-centered">کد ارائه</th>
+                    <th class="has-text-centered">نام درس</th>
+                    <th class="has-text-centered">نام استاد</th>
+                    <th class="has-text-centered">تاریخ برگزاری امتحان</th>
+                    <th class="has-text-centered">نمره</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="lesson in karnameh" :key="lesson.nemeh_dars" class="has-text-centered">
+                    <td v-for="less in lesson" :key="less" class="has-text-centered">{{less}}</td>
+                </tr>
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+        </table>
     </div>
 </template>
 
 <script>
+    import {
+        mapGetters
+    } from 'vuex';
+
     export default {
-       
+        data() {
+            return {
+                karnameh: [],
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'chosedUnits'
+            ]),
+
+        },
+        created() {
+            this.chosedUnits.forEach(element => {
+                let dars = {
+                    code_dars: element.code_dars,
+                    code_erae: element.code_erae,
+                    name_dars: element.name_dars,
+                    ostad_name: element.ostad_name,
+                    tarikh_emtehan: element.tarikh_emtehan,
+                    nomreh: 'منظور نشده'
+                };
+                this.karnameh.push(dars);
+            });
+        }
     }
 </script>
 
